ec78c96d7277aead0212c64e1c37047039541eec
Param <param@mac.com>
2023-09-15 11:00:15 +0530

    Updated Swiper

--- README.md
+++ README.md
@@ -16,57 +16,30 @@ npm i @cloudparker/easy-swiper-svelte --save-dev
 
 ```ts
 <script lang="ts">
-	import EasyScriptLoader from '@cloudparker/easy-script-loader.svelte';
-
-	let swiperRef: HTMLDivElement;
-
-	function handleSwipperLoad(ev: CustomEvent) {
-		let Swiper: any = ev.detail;
-		const swiper = new Swiper(swiperRef, {
-			slidesPerView: 3,
-			spaceBetween: 8,
-			freeMode: true,
-			pagination: {
-				clickable: true
-			}
-		});
-	}
+	import EasySwiper from '@cloudparker/easy-swiper-svelte';
 </script>
 
 <div>
-	<EasySwiper
-		on:load={handleSwipperLoad}
-	>
-		<div class="swiper" bind:this={swiperRef}>
-			<div class="swiper-wrapper">
-				<div class="swiper-slide">Slide 1</div>
-				<div class="swiper-slide">Slide 2</div>
-				<div class="swiper-slide">Slide 3</div>
-				<div class="swiper-slide">Slide 4</div>
-				<div class="swiper-slide">Slide 5</div>
-				<div class="swiper-slide">Slide 6</div>
-				<div class="swiper-slide">Slide 7</div>
-				<div class="swiper-slide">Slide 8</div>
-				<div class="swiper-slide">Slide 9</div>
-				<div class="swiper-slide">Slide 10</div>
-			</div>
-		</div>
-	</EasyScriptLoader>
+	<EasySwiper size={10}>
+		<div slot="slide" let:index>Slide {index}</div>
+	</EasySwiper>
 </div>
 
 <style>
-	.swiper {
+	:global(.swiper) {
 		width: 600px;
 		height: 300px;
 		border: 1px solid grey;
 	}
-	.swiper-slide {
+
+	:global(.swiper-slide) {
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		background-color: aqua;
 	}
 </style>
+
 
 ```
 
--- src/lib/easy-swiper.svelte
+++ src/lib/easy-swiper.svelte
@@ -1,9 +1,69 @@
 <script lang="ts">
 	import EasyScriptLoader from '@cloudparker/easy-script-loader-svelte';
-	export let script: string = 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js';
-	export let style: string = 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css';
+	import { createEventDispatcher } from 'svelte';
+	export let scriptUrl: string = 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js';
+	export let styleUrl: string = 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css';
+	export let scriptName: string = 'Swiper';
+	export let swiperClass: string = '';
+	export let swiperWrapperClass: string = '';
+	export let swiperStyle: string = '';
+	export let swiperWrapperStyle: string = '';
+	export let swiperPaginationClass: string = '';
+	export let swiperPaginationStyle: string = '';
+	export let swiperNextBtnClass: string = '';
+	export let swiperNextBtnStyle: string = '';
+	export let swiperPrevBtnClass: string = '';
+	export let swiperPrevBtnStyle: string = '';
+	export let swiperScrollbarClass: string = '';
+	export let swiperScrollbarStyle: string = '';
+	export let swiperSlideClass: string = '';
+	export let swiperSlideStyle: string = '';
+
+	export let hasPagination: boolean = false;
+	export let hasControlBtns: boolean = false;
+	export let hasScrollbar: boolean = false;
+
+	export let size: number = 0;
+
+	export let swiperOptions: any = {
+		slidesPerView: 3,
+		spaceBetween: 8,
+		freeMode: true,
+		pagination: {
+			clickable: true
+		}
+	};
+
+	let swiperRef: HTMLDivElement;
+	let dispatch = createEventDispatcher();
+
+	function handleSwipperLoad(ev: CustomEvent) {
+		let Swiper: any = ev.detail;
+		const swiper = new Swiper(swiperRef, swiperOptions);
+		dispatch('load', { Swiper, swiper });
+	}
 </script>
 
-<EasyScriptLoader scriptName="Swiper" {script} {style} on:load>
-	<slot />
+<EasyScriptLoader {scriptName} {scriptUrl} {styleUrl} on:load={handleSwipperLoad}>
+	<div class="swiper {swiperClass}" bind:this={swiperRef} style=" {swiperStyle}">
+		<div class="swiper-wrapper {swiperWrapperClass}" style=" {swiperWrapperStyle}">
+			{#each Array(size) as _, index}
+				<div class="swiper-slide {swiperSlideClass}" style=" {swiperSlideStyle}">
+					<slot name="slide" {index} />
+				</div>
+			{/each}
+		</div>
+		{#if hasPagination}
+			<div class="swiper-pagination {swiperPaginationClass}" style=" {swiperPaginationStyle}"></div>
+		{/if}
+
+		{#if hasControlBtns}
+			<div class="swiper-button-prev {swiperPrevBtnClass}" style=" {swiperPrevBtnStyle}"></div>
+			<div class="swiper-button-next {swiperNextBtnClass}" style=" {swiperNextBtnStyle}"></div>
+		{/if}
+
+		{#if hasScrollbar}
+			<div class="swiper-scrollbar {swiperScrollbarClass}" style=" {swiperScrollbarStyle}"></div>
+		{/if}
+	</div>
 </EasyScriptLoader>
